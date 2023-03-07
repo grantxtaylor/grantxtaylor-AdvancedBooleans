47cdd937b415f67451a82d1e0aac51ad7a3a92d3
grantxtaylor <xgrtaylor@gmail.com>
2023-03-07 16:05:04 +0000

    d

--- src/main/java/AdvancedBooleanLogic.java
+++ src/main/java/AdvancedBooleanLogic.java
@@ -29,7 +29,7 @@ public class AdvancedBooleanLogic {
     }
 
     /**
-     * Neither b1 NOR b2,,
+     * Neither b1 NOR b2,,.
      * @param b1 first boolean to be compared.
      * @param b2 second boolean to be compared.
      * @return true if neither b1 nor b2 are true. Return false if either b1 or b2 is true.
